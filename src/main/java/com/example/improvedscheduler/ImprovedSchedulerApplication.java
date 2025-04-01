13717289b2dfd7fb95ccf1c53a96c93c863d27d0
ksr-ccb <harang4282@gmail.com>
2025-04-01 13:28:37 +0900

    🎉 Begin a project [improved Scheduler]

---
+++ src/main/java/com/example/improvedscheduler/ImprovedSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.example.improvedscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImprovedSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImprovedSchedulerApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/improvedscheduler/entity.java
@@ -0,0 +1,4 @@
+package com.example.improvedscheduler;
+
+public class entity {
+}
