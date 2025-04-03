4935ea43e938b03abfa9daf7b73249f41c7c861b
ksr-ccb <harang4282@gmail.com>
2025-04-03 17:23:14 +0900

    ✨ Add Logout

--- src/main/java/com/example/improvedscheduler/controller/user/UserController.java
+++ src/main/java/com/example/improvedscheduler/controller/user/UserController.java
@@ -25,7 +25,7 @@ public class UserController {
     private final UserService userService;
 
     /**
-     * 새로운 유저를 만들기 위한 함수입니다.
+     * 새로운 유저를 만들기 위한 API 입니다.
      * POST METHOD를 통해서 RequestBody 의 Json 내용을 SignUpRequestDto 으로 변환하여 가져옵니다.
      * @param requestDto 회원가입 위해 필요한 변수는 '이름, 비밀번호, 이메일' 세가지 입니다.
      * @return 가입 정상 완료시 SignUpResponseDto 반환.(이름, 이메일, 회원번호)
@@ -45,7 +45,7 @@ public class UserController {
     }
 
     /**
-     * 로그인 하는 함수입니다.
+     * 로그인 하는 API 입니다.
      * 입력받은 이메일과 비밀번호가 모두 일치하는 데이터가 있는 경우
      * 세션에 해당 유저의 정보를 저장합니다(아이디, 이름, 이메일)
      */
@@ -63,8 +63,7 @@ public class UserController {
     }
 
     /**
-     * 현재 로그인한 사용자의 정보를 조회하는 엔드포인트입니다.
-     *
+     * 현재 로그인한 사용자의 정보를 조회하는 API 입니다.
      * @param request 클라이언트의 HTTP 요청 객체로, 세션 정보를 포함합니다.
      * @return 로그인한 사용자의 정보를 담은 {@link UserResponseDto} 객체와 HTTP 상태 코드 200(OK)
      *         필터를통해 이미 로그인이 되어있는 것을 확인했기 때문에 바로 세션을 받아옵니다.
@@ -81,7 +80,7 @@ public class UserController {
     }
 
     /**
-     * 사용자의 정보를 업데이트하는 엔드포인트입니다.
+     * 사용자의 정보를 업데이트하는 API 입니다.
      * 사용자 이름, 비밀번호를 선택적으로 변경할 수 있습니다.
      * @param id 업데이트할 사용자 ID
      * @param requestDto 업데이트할 사용자 정보가 담긴 DTO 객체
@@ -101,7 +100,7 @@ public class UserController {
     }
 
     /**
-     * 특정 사용자를 삭제하는 API 엔드포인트입니다.
+     * 특정 사용자를 삭제하는 API 입니다.
      * 요청된 사용자 ID에 해당하는 사용자를 삭제합니다.
      * 사용자가 작성한 모든 스케줄도 함께 삭제됩니다.
      * @param userId 삭제할 사용자의 ID
@@ -111,6 +110,25 @@ public class UserController {
     public ResponseEntity<Void> deleteUser(
             @PathVariable Long userId){
         userService.deleteUser(userId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    /**
+     * 로그아웃을 하는 API 입니다.
+     * @param request 현재 로그인 한 세션 값
+     * @return HTTP 204 No Content 상태 코드 반환 (응답 본문 없음)
+     */
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(
+            HttpServletRequest request
+    ) {
+
+        HttpSession session = request.getSession(false);
+        // 세션이 존재하면 -> 로그인이 된 경우
+        if(session != null) {
+            session.invalidate(); // 해당 세션(데이터)을 삭제한다.
+        }
+
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/com/example/improvedscheduler/repository/schedule/ScheduleRepository.java
+++ src/main/java/com/example/improvedscheduler/repository/schedule/ScheduleRepository.java
@@ -2,11 +2,8 @@ package com.example.improvedscheduler.repository.schedule;
 
 import com.example.improvedscheduler.entity.schedule.Schedule;
 import com.example.improvedscheduler.exception.ResourceNotFoundException;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -15,8 +12,6 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     default Schedule findByIdOrElseThrow(Long id) {
         return findById(id).orElseThrow(() -> new ResourceNotFoundException("해당 아이디 값이 존재하지 않습니다. : " + id));
     }
-
-    Page<Schedule> findAllByOrderByIdAsc(Pageable pageable);
 
     List<Schedule> findAllByUser_Id(Long id);
 
